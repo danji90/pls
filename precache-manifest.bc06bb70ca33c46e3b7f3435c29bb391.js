2eaa9b493b1e41ac32e021b1334ab2de6b03d7e9
danji90 <danji_ma90@hotmail.com>
2020-06-04 00:51:19 +0200

    Updates

---
+++ precache-manifest.bc06bb70ca33c46e3b7f3435c29bb391.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c325c9c34ceac4b130dcaa7ea2d776e",
+    "url": "/pls/index.html"
+  },
+  {
+    "revision": "3dd0394a3a4184f4f721",
+    "url": "/pls/static/css/main.4a0c58ea.chunk.css"
+  },
+  {
+    "revision": "4c1ef2f6b37f95f7ac07",
+    "url": "/pls/static/js/2.d2ed663a.chunk.js"
+  },
+  {
+    "revision": "3dd0394a3a4184f4f721",
+    "url": "/pls/static/js/main.3dcecb3b.chunk.js"
+  },
+  {
+    "revision": "295a7c3f784ff9518f54",
+    "url": "/pls/static/js/runtime-main.e4849d10.js"
+  },
+  {
+    "revision": "f338da7c2887a762987cd9f3fef29543",
+    "url": "/pls/static/media/desk.f338da7c.jpg"
+  }
+]);
