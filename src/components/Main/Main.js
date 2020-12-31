c6c640b95c1c6975e6bf176c5df0fd2e852ab342
danji90 <danji_ma90@hotmail.com>
2020-12-31 17:37:20 +0100

    scroll to top on router change

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -14,6 +14,7 @@ import Individual from '../Individual/Individual';
 import Professional from '../Professional/Professional';
 import Business from '../Business/Business';
 import LevelTest from '../LevelTest/LevelTest';
+import ScrollToTop from '../ScrollToTop/ScrollToTop';
 import conversation from '../../utils/images/study_group2.jpg';
 import workshop from '../../utils/images/workshop2.jpg';
 import business from '../../utils/images/business.jpg';
@@ -26,50 +27,52 @@ const Main = () => {
         {/* Router */}
         <div style={{ position: 'relative' }}>
           <NavBar />
-          <Switch>
-            <Route exact path="/">
-              <Redirect to="/home" />
-            </Route>
-            <Route exact path="/home">
-              <TabPanel index={0}>
-                <Home />
-              </TabPanel>
-              <Footer />
-            </Route>
-            <Route path="/individual">
-              <TabPanel index={1} backgroundImage={conversation}>
-                <Individual />
-              </TabPanel>
-              <Footer />
-            </Route>
-            <Route path="/professional">
-              <TabPanel index={2} backgroundImage={workshop}>
-                <Professional />
+          <ScrollToTop>
+            <Switch>
+              <Route exact path="/">
+                <Redirect to="/home" />
+              </Route>
+              <Route exact path="/home">
+                <TabPanel index={0}>
+                  <Home />
+                </TabPanel>
                 <Footer />
-              </TabPanel>
-            </Route>
-            <Route path="/business">
-              <TabPanel index={3} backgroundImage={business}>
-                <Business />
+              </Route>
+              <Route path="/individual">
+                <TabPanel index={1} backgroundImage={conversation}>
+                  <Individual />
+                </TabPanel>
                 <Footer />
-              </TabPanel>
-            </Route>
-            <Route path="/test">
-              <TabPanel index={4} backgroundImage={test}>
-                <LevelTest />
-                <Footer />
-              </TabPanel>
-            </Route>
-            <Route path="/contact">
-              <TabPanel index={5}>
-                Contact Form
-                <Footer />
-              </TabPanel>
-            </Route>
-            <Route exact path="/*">
-              <Redirect to="/home" />
-            </Route>
-          </Switch>
+              </Route>
+              <Route path="/professional">
+                <TabPanel index={2} backgroundImage={workshop}>
+                  <Professional />
+                  <Footer />
+                </TabPanel>
+              </Route>
+              <Route path="/business">
+                <TabPanel index={3} backgroundImage={business}>
+                  <Business />
+                  <Footer />
+                </TabPanel>
+              </Route>
+              <Route path="/test">
+                <TabPanel index={4} backgroundImage={test}>
+                  <LevelTest />
+                  <Footer />
+                </TabPanel>
+              </Route>
+              <Route path="/contact">
+                <TabPanel index={5}>
+                  Contact Form
+                  <Footer />
+                </TabPanel>
+              </Route>
+              <Route exact path="/*">
+                <Redirect to="/home" />
+              </Route>
+            </Switch>
+          </ScrollToTop>
         </div>
       </Router>
     </>
